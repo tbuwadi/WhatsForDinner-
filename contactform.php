2d050876b8a711fb6950d09870418b743f6e3bc9
Tala <tbuwadi@gmail.com>
2020-05-12 03:28:19 -0400

    first commit

--- contactform.php
+++ contactform.php
@@ -1,10 +1,47 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Dinner Requests</title>
+        <link rel="stylesheet" type="text/css" href="styles2.css">
+        <link href="https://fonts.googleapis.com/css?family=Concert+One|Fredoka+One&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Patua+One&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Thank you for sending!</h1>
+        <img id="image" src="mama2.png"/>
 
-if(isset($_POST['submit'])){
-    $message = $_POST['message'];
-    $subject = "";
-    $mailTo = "5197098018@vmobile.ca";
+        <?php
+        require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
+        
+        try {
+            // Create the Transport
+            $transport = (new Swift_SmtpTransport('smtp.googlemail.com', 465, 'ssl'))
+              ->setUsername('tbuwad@gmail.com')
+              ->setPassword('xxxxxxxx')
+            ;
+        
+            // Create the Mailer using your created Transport
+            $mailer = new Swift_Mailer($transport);
+        
+            // Create a message
+            $body = $_POST['message'];
+        
+            $message = (new Swift_Message('New Dinner Request'))
+              ->setFrom(['tbuwad@gmail.com' => 'Anonymous'])
+              ->setTo(['xxxxxxxxxx@vmobile.ca'])
+              ->setBody($body)
+              ->setContentType('text/html')
+            ;
+        
+            // Send the message
+            $mailer->send($message);
 
-    mail($mailTo, $subject, $message);
-    header("Location: index.php?mailsend");
-}
+        } catch(Exception $e) {
+            echo $e->getMessage();
+        }
+        ?>
+
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,16 +1,19 @@
-<html>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Fathieh's Dinner Requests</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Dinner Requests</title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <link href="https://fonts.googleapis.com/css?family=Concert+One|Fredoka+One&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Patua+One&display=swap" rel="stylesheet">
     </head>
     <body>
-        <main>
-            <form action="contactform.php" method="post">
-                <input type="text" name="message" placeholder="Enter Dinner Suggestion">
-                <button type="submit" name="submit">Send to Fathieh</button>
-            </form>
-        </main>
+        <img id="image" src="mama.png"/>
+        <h1>Mama's Dinner </br> Suggestions</h1>
+        <form id="form" action="contactform.php" method="post">
+            <input autocomplete="off" type="text" name="message" placeholder="I don't know what to cook!"></br>
+            <button id="button" type="submit" name="submit">Send to Mama</button>
+        </form>
     </body>
 </html>
